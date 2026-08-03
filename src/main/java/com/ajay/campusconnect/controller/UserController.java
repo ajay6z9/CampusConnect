4ade9a882e3af6a5fe7ea2fabd9c4f30c3441b57
ajay6z9 <ajayvinay9926@gmail.com>
2026-08-03 23:10:25 +0530

    Implemented user registration API

--- src/main/java/com/ajay/campusconnect/controller/UserController.java
+++ src/main/java/com/ajay/campusconnect/controller/UserController.java
@@ -1,4 +1,23 @@
 package com.ajay.campusconnect.controller;
 
+import com.ajay.campusconnect.dto.UserRequest;
+import com.ajay.campusconnect.entity.User;
+import com.ajay.campusconnect.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User register(@Valid @RequestBody UserRequest request) {
+        return userService.register(request);
+    }
 }
--- src/main/java/com/ajay/campusconnect/service/UserService.java
+++ src/main/java/com/ajay/campusconnect/service/UserService.java
@@ -1,4 +1,32 @@
 package com.ajay.campusconnect.service;
 
+import com.ajay.campusconnect.dto.UserRequest;
+import com.ajay.campusconnect.entity.User;
+import com.ajay.campusconnect.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User register(UserRequest request) {
+
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        User user = User.builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .password(request.getPassword())
+                .phone(request.getPhone())
+                .build();
+
+        return userRepository.save(user);
+    }
 }
