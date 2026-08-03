953b0b8df12360f86c2ae7b27006805198253f94
ajay6z9 <ajayvinay9926@gmail.com>
2026-08-04 00:13:37 +0530

    Implemented secure authentication foundation

--- src/main/java/com/ajay/campusconnect/dto/UserRequest.java
+++ src/main/java/com/ajay/campusconnect/dto/UserRequest.java
@@ -1,21 +1,27 @@
 package com.ajay.campusconnect.dto;
+//package com.ajay.campusconnect.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserRequest {
 
-    @NotBlank(message = "Name cannot be empty")
+    @NotBlank(message = "Name is required")
     private String name;
 
-    @Email(message = "Enter a valid email")
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
     private String email;
 
-    @NotBlank(message = "Password cannot be empty")
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must contain at least 8 characters")
     private String password;
 
-    @NotBlank(message = "Phone cannot be empty")
+    @Pattern(regexp = "^[6-9]\\d{9}$",
+            message = "Enter a valid 10-digit mobile number")
     private String phone;
 }
--- src/main/java/com/ajay/campusconnect/service/UserService.java
+++ src/main/java/com/ajay/campusconnect/service/UserService.java
@@ -1,17 +1,24 @@
 package com.ajay.campusconnect.service;
+
 import com.ajay.campusconnect.dto.UserRequest;
 import com.ajay.campusconnect.dto.UserResponse;
 import com.ajay.campusconnect.entity.User;
 import com.ajay.campusconnect.exception.EmailAlreadyExistsException;
 import com.ajay.campusconnect.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder) {
+
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public UserResponse register(UserRequest request) {
@@ -23,7 +30,7 @@ public class UserService {
         User user = User.builder()
                 .name(request.getName())
                 .email(request.getEmail())
-                .password(request.getPassword())
+                .password(passwordEncoder.encode(request.getPassword()))
                 .phone(request.getPhone())
                 .build();
 
