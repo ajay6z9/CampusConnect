8417586f17dbbbb24bb9310476f3837d87e516f9
ajay6z9 <ajayvinay9926@gmail.com>
2026-08-04 12:08:34 +0530

    Implemented Ride CRUD operations

--- src/main/java/com/ajay/campusconnect/controller/RideController.java
+++ src/main/java/com/ajay/campusconnect/controller/RideController.java
@@ -5,7 +5,7 @@ import com.ajay.campusconnect.dto.RideResponse;
 import com.ajay.campusconnect.service.RideService;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.List;
 @RestController
 @RequestMapping("/api/rides")
 public class RideController {
@@ -21,5 +21,27 @@ public class RideController {
             @Valid @RequestBody RideRequest request) {
 
         return rideService.createRide(request);
+    }
+    @GetMapping
+    public List<RideResponse> getAllRides() {
+        return rideService.getAllRides();
+    }
+    @GetMapping("/{id}")
+    public RideResponse getRideById(@PathVariable Long id) {
+        return rideService.getRideById(id);
+    }
+    @PutMapping("/{id}")
+    public RideResponse updateRide(
+            @PathVariable Long id,
+            @Valid @RequestBody RideRequest request) {
+
+        return rideService.updateRide(id, request);
+    }
+    @DeleteMapping("/{id}")
+    public String deleteRide(@PathVariable Long id) {
+
+        rideService.deleteRide(id);
+
+        return "Ride deleted successfully";
     }
 }
--- src/main/java/com/ajay/campusconnect/service/RideService.java
+++ src/main/java/com/ajay/campusconnect/service/RideService.java
@@ -7,7 +7,7 @@ import com.ajay.campusconnect.entity.User;
 import com.ajay.campusconnect.repository.RideRepository;
 import com.ajay.campusconnect.repository.UserRepository;
 import org.springframework.stereotype.Service;
-
+import java.util.List;
 @Service
 public class RideService {
 
@@ -48,5 +48,72 @@ public class RideService {
                 .description(savedRide.getDescription())
                 .driverName(savedRide.getDriver().getName())
                 .build();
+    }
+    public List<RideResponse> getAllRides() {
+
+        return rideRepository.findAll()
+                .stream()
+                .map(ride -> RideResponse.builder()
+                        .id(ride.getId())
+                        .source(ride.getSource())
+                        .destination(ride.getDestination())
+                        .departureTime(ride.getDepartureTime())
+                        .availableSeats(ride.getAvailableSeats())
+                        .price(ride.getPrice())
+                        .description(ride.getDescription())
+                        .driverName(ride.getDriver().getName())
+                        .build())
+                .toList();
+    }
+    public RideResponse getRideById(Long id) {
+
+        Ride ride = rideRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Ride not found"));
+
+        return RideResponse.builder()
+                .id(ride.getId())
+                .source(ride.getSource())
+                .destination(ride.getDestination())
+                .departureTime(ride.getDepartureTime())
+                .availableSeats(ride.getAvailableSeats())
+                .price(ride.getPrice())
+                .description(ride.getDescription())
+                .driverName(ride.getDriver().getName())
+                .build();
+    }
+    public RideResponse updateRide(Long id, RideRequest request) {
+
+        Ride ride = rideRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Ride not found"));
+
+        ride.setSource(request.getSource());
+        ride.setDestination(request.getDestination());
+        ride.setDepartureTime(request.getDepartureTime());
+        ride.setAvailableSeats(request.getAvailableSeats());
+        ride.setPrice(request.getPrice());
+        ride.setDescription(request.getDescription());
+
+        Ride updatedRide = rideRepository.save(ride);
+
+        return RideResponse.builder()
+                .id(updatedRide.getId())
+                .source(updatedRide.getSource())
+                .destination(updatedRide.getDestination())
+                .departureTime(updatedRide.getDepartureTime())
+                .availableSeats(updatedRide.getAvailableSeats())
+                .price(updatedRide.getPrice())
+                .description(updatedRide.getDescription())
+                .driverName(updatedRide.getDriver().getName())
+                .build();
+    }
+    public void deleteRide(Long id) {
+
+        Ride ride = rideRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Ride not found"));
+
+        rideRepository.delete(ride);
     }
 }
