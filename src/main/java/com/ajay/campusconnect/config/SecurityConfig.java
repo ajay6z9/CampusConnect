46dcd67c043fa879c5a26d550f50739462acfc0a
ajay6z9 <ajayvinay9926@gmail.com>
2026-08-04 21:30:54 +0530

    Added CORS configuration and updated security config

--- src/main/java/com/ajay/campusconnect/config/SecurityConfig.java
+++ src/main/java/com/ajay/campusconnect/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors ->{})
 
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(
